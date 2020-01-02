14a56864773177bb3af665805af8c2349bed39ac
Reinhold Früsmer <reinhold.fruesmer@tt-s.com>
2020-01-02 08:47:39 +0100

    Testing indentation setting

--- src/rfx/rfx.cpp
+++ src/rfx/rfx.cpp
@@ -17,36 +17,37 @@ using namespace std;
 #ifdef _WINDOWS
 
 int APIENTRY wWinMain(HINSTANCE instanceHandle,
-                     HINSTANCE prevInstance,
-                     LPWSTR commandLine,
-                     int showParam)
+	HINSTANCE prevInstance,
+	LPWSTR commandLine,
+	int showParam)
 {
 	try
 	{
 		RFX_LOG_INFO << "Starting rfx application ...";
 
 		Logger::setLogLevel(LogLevel::TRACE);
-		
+
 		Win32Application theApp(instanceHandle);
 		theApp.run();
 
 		return EXIT_SUCCESS;
 	}
-	catch (const exception& ex)
+	catch (const exception & ex)
 	{
-        RFX_LOG_FATAL << ex.what();
+		RFX_LOG_FATAL << ex.what();
 	}
 	catch (...)
 	{
 		RFX_LOG_FATAL << "Unknown exception";
 	}
-	
+
 	return EXIT_FAILURE;
 }
 
 #else // _WINDOWS
 static_assert(false, "Not implemented");
 #endif // _WINDOWS
+
 
 
 
